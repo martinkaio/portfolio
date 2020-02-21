7b0bd43bd436b6bc7b57c0a8292aad9e105c5a4a
kTasuja <kaiomartin0@gmail.com>
2020-02-21 18:30:13 +0000

    Set up express

--- server.js
+++ server.js
@@ -2,3 +2,12 @@ const express = require("express");
 const path = require("path");
 const cookieSession = require("cookie-session");
 const createError = require("http-errors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const port = 3000;
+
+app.set("trust proxy", 1);
+
+app.use(cookieSession({ name: "session" }));
