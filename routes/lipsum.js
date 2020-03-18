24c6a6b91b4e6bc44a2c96cdc855683b85c8aa04
kTasuja <kaiomartin0@gmail.com>
2020-03-18 11:12:16 +0000

    Add lipsum page

--- routes/lipsum.js
+++ routes/lipsum.js
@@ -1,19 +1,14 @@
 const express = require("express");
 
-const lipsumRoute = require("./lipsum");
-
 const router = express.Router();
 
 module.exports = () => {
   router.get("/", (request, response, next) => {
     try {
-      response.render("pages/index", { pageTitle: "Welcome" });
+      response.render("pages/lipsum", { pageTitle: "Lipsum" });
     } catch (err) {
       return next(err);
     }
   });
-
-  router.use("/lipsum", lipsumRoute());
-
   return router;
 };
