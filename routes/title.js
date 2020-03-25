80ec06f3707b1342d0233fe68001fbb96e66b7aa
kTasuja <kaiomartin0@gmail.com>
2020-03-25 23:00:20 +0000

    Add test page for final version, new navbar

--- routes/title.js
+++ routes/title.js
@@ -1,21 +1,14 @@
 const express = require("express");
 
-const lipsumRoute = require("./lipsum");
-const titleRoute = require("./title");
-
 const router = express.Router();
 
 module.exports = () => {
   router.get("/", (request, response, next) => {
     try {
-      response.render("pages/index", { pageTitle: "Welcome" });
+      response.render("pages/title", { pageTitle: "Title" });
     } catch (err) {
       return next(err);
     }
   });
-
-  router.use("/lipsum", lipsumRoute());
-  router.use("/title", titleRoute());
-
   return router;
 };
