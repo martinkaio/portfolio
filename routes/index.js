240f13559b563fca501132620091f0206eed6bf5
kTasuja <kaiomartin0@gmail.com>
2020-03-27 21:20:52 +0000

    Add pageview counter

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,67 @@ const router = express.Router();
 
 module.exports = () => {
   router.get("/", (request, response, next) => {
+    const initDb = function(callback) {
+      if (request.mongoURL == null) return;
+
+      const mongodb = require("mongodb");
+      if (mongodb == null) return;
+
+      mongodb.connect(mongoURL, function(err, conn) {
+        if (err) {
+          callback(err);
+          return;
+        }
+
+        db = conn;
+        dbDetails.databaseName = db.databaseName;
+        dbDetails.url = request.mongoURLLabel;
+        dbDetails.type = "MongoDB";
+
+        console.log("Connected to MongoDB at: %s", request.mongoURL);
+      });
+    };
+
     try {
-      response.render("pages/index", { pageTitle: "Welcome" });
+      var db = request.db;
+      if (!db) {
+        initDb(function(err) {});
+      }
+      if (db) {
+        var col = db.collection("counts");
+        // Create a document with request IP and current time of request
+        col.insert({ ip: request.ip, date: Date.now() });
+        col.count(function(err, count) {
+          if (err) {
+            console.log("Error running count. Message:\n" + err);
+          }
+          response.render("pages/index", {
+            pageTitle: "Welcome",
+            pageCountMessage: count,
+            dbInfo: dbDetails
+          });
+        });
+      } else {
+        response.render("pages/index", {
+          pageTitle: "Welcome",
+          pageCountMessage: null
+        });
+      }
+
+      app.get("/pagecount", function(req, res) {
+        // try to initialize the db on every request if it's not already
+        // initialized.
+        if (!db) {
+          initDb(function(err) {});
+        }
+        if (db) {
+          db.collection("counts").count(function(err, count) {
+            res.send("{ pageCount: " + count + "}");
+          });
+        } else {
+          res.send("{ pageCount: -1 }");
+        }
+      });
     } catch (err) {
       return next(err);
     }
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const path = require("path");
 const cookieSession = require("cookie-session");
 const createError = require("http-errors");
 const routes = require("./routes");
+Object.assign = require("object-assign");
 
 const app = express();
 
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
+const mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+var mongoURLLabel = "";
 
 app.set("trust proxy", 1);
 
@@ -18,6 +21,67 @@ app.use(
   })
 );
 
+if (mongoURL == null) {
+  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
+  // If using plane old env vars via service discovery
+  if (process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+    mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
+    mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
+    mongoDatabase = process.env[mongoServiceName + "_DATABASE"];
+    mongoPassword = process.env[mongoServiceName + "_PASSWORD"];
+    mongoUser = process.env[mongoServiceName + "_USER"];
+
+    // If using env vars from secret from service binding
+  } else if (process.env.database_name) {
+    mongoDatabase = process.env.database_name;
+    mongoPassword = process.env.password;
+    mongoUser = process.env.username;
+    var mongoUriParts = process.env.uri && process.env.uri.split("//");
+    if (mongoUriParts.length == 2) {
+      mongoUriParts = mongoUriParts[1].split(":");
+      if (mongoUriParts && mongoUriParts.length == 2) {
+        mongoHost = mongoUriParts[0];
+        mongoPort = mongoUriParts[1];
+      }
+    }
+  }
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = "mongodb://";
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ":" + mongoPassword + "@";
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+    mongoURL += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+  }
+}
+
+var db = null;
+var dbDetails = new Object();
+
+var initDb = function(callback) {
+  if (mongoURL == null) return;
+
+  var mongodb = require("mongodb");
+  if (mongodb == null) return;
+
+  mongodb.connect(mongoURL, function(err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    db = conn;
+    dbDetails.databaseName = db.databaseName;
+    dbDetails.url = mongoURLLabel;
+    dbDetails.type = "MongoDB";
+
+    console.log("Connected to MongoDB at: %s", mongoURL);
+  });
+};
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 
@@ -26,6 +90,27 @@ app.locals.siteName = "Martin Kaio";
 app.use(express.static(path.join(__dirname, "./static")));
 
 app.use("/", routes());
+
+app.get("/", function(req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err) {});
+  }
+  if (db) {
+    var col = db.collection("counts");
+    // Create a document with request IP and current time of request
+    col.insert({ ip: req.ip, date: Date.now() });
+    col.count(function(err, count) {
+      if (err) {
+        console.log("Error running count. Message:\n" + err);
+      }
+      res.render("index.html", { pageCountMessage: count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render("index.html", { pageCountMessage: null });
+  }
+});
 
 app.use((request, response, next) => {
   return next(createError(404, "File not found"));
@@ -38,6 +123,10 @@ app.use((err, request, response, next) => {
   response.locals.status = status;
   response.status(status);
   response.render("error");
+});
+
+initDb(function(err) {
+  console.log("Error connecting to Mongo. Message:\n" + err);
 });
 
 app.listen(port, ip, () => {
