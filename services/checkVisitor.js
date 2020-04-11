ea18def6637085102b626559a64ae787600666d2
kTasuja <kaiomartin0@gmail.com>
2020-04-11 23:26:48 +0100

    Remove unnecessary code

--- services/checkVisitor.js
+++ services/checkVisitor.js
@@ -2,30 +2,16 @@ const checkVisitor = async function(visitors, visitorIp) {
   if (
     (await visitors
       .find({
-        ip: visitorIp
-      })
-      .limit(1)
-      .count()) < 1
-  ) {
-    console.log("new IP");
-    col.insertOne({
-      ip: visitorIp,
-      date: Date.now(),
-      visits: 1
-    });
-  } else if (
-    (await visitors
-      .find({
         ip: visitorIp,
         date: { $gt: Date.now() - 50000 }
       })
       .limit(1)
       .count()) < 1
   ) {
-    console.log("new visit");
-    visitors.updateOne(
+    await visitors.updateOne(
       { ip: visitorIp },
-      { $set: { date: Date.now() }, $inc: { visits: 1 } }
+      { $set: { date: Date.now() }, $inc: { visits: 1 } },
+      { upsert: true }
     );
   }
 };
