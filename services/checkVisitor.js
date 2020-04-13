9236ca430928336fcf60a09b9442634cbef4305b
kTasuja <kaiomartin0@gmail.com>
2020-04-14 00:48:40 +0100

    Style changes

--- services/checkVisitor.js
+++ services/checkVisitor.js
@@ -3,7 +3,7 @@ const checkVisitor = async function(visitors, visitorIp) {
     (await visitors
       .find({
         ip: visitorIp,
-        date: { $gt: Date.now() - 900000 }
+        date: { $gt: Date.now() - 15 * 60 * 1000 }
       })
       .limit(1)
       .count()) < 1
